3c38d2146f6c2d07152c2dfc1c185976fa87e941
tingru0515 <7687@holbertonstudents.com>
2024-01-16 02:56:11 -0800

    q1-4

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,9 @@ typedef struct levelorder_queue_s
 
 /* task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif
